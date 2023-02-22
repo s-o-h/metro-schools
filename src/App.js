3e670a9683877045f13b550bab6cb8b016b49529
S Oh <11490947+s-o-h@users.noreply.github.com>
2023-02-22 12:14:49 -0800

    Add schools data to map, adjust center and zoom

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import schoolsData from "./schools.json";
 function App() {
   console.log(schoolsData);
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[45.4, -122.6819]} zoom={10} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -23,6 +23,7 @@ function App() {
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
+      <GeoJSON data={schoolsData.features} />
     </MapContainer>
   );
 }
